077e4d926ee123bbaa2838f458ea65f198a8fed7
Patlisan <saysansay@yahoo.com>
2021-05-25 14:48:01 +0700

    Create Tugas4.php

---
+++ Tugas4.php
@@ -0,0 +1,31 @@
+<?php
+function createSalt()
+{
+    $text = md5(uniqid(rand(), TRUE));
+    return substr($text, 0, 3);
+}
+function createSHA256($password,$salt){
+    return hash('sha256',$salt.$password);
+}
+function verifyHash($hash,$password,$salt)
+{
+  $verifyHash = createSHA256($password,$salt);
+
+  if($hash==$verifyHash)
+  {
+      return "true";
+  }else {
+      return "false";
+  }
+}
+$password ="UBL";
+$salt=createSalt();
+$hashPwd = createSHA256($password,$salt);
+$verify = verifyHash($hashPwd,$password,$salt);
+echo "Password :".$password;
+echo "<br>";
+echo "Salt : ".$salt;
+echo "<br>";
+echo "Hash : ".$hashPwd;
+echo "<br>";
+echo "Verify Text : UBL  ".$verify;
